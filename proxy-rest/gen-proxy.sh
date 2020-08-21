fcbb365d096b9cb87e28d74c6243cc973c48857d
kgignatyev <kgignatyev@gmail.com>
2020-08-21 11:19:52 -0700

    adjusted REST annotations; updated slides;

--- proxy-rest/gen-proxy.sh
+++ proxy-rest/gen-proxy.sh
@@ -3,10 +3,13 @@ mkdir -p gen/swagger
 mkdir -p gen/go
 
 protoc -I ./proto_deps/ -I ../interface/src/main/proto/ \
-   --grpc-gateway_out=logtostderr=true,paths=source_relative:./gen/go/ \
-   --go_out=plugins=grpc,paths=source_relative:./gen/go/ \
+   --grpc-gateway_out=logtostderr=true,paths=source_relative:./kgi_geography_api/ \
+   --go_out=plugins=grpc,paths=source_relative:./kgi_geography_api/ \
    --swagger_out=logtostderr=true:./gen/swagger \
   geography.proto
 
+export BIN_TARGET=$HOME/go/bin/kgi_geography_rest_proxy
 
-go build -i -o $HOME/go/bin/kgi_geography_rest_proxy
+go build -i -o $BIN_TARGET
+
+echo "rest proxy built at $BIN_TARGET"
